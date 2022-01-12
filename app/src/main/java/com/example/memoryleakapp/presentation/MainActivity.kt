65d0cc99359b51fa0113f998d030d1a738e3233c
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2022-01-12 20:22:10 -0300

    move main activity to presentation package

--- app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.memoryleakapp
+package com.example.memoryleakapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.memoryleakapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
