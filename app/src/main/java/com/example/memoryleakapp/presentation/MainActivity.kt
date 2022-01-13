29fc29d1e7ad147d267885d963e3dd75594f805c
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2022-01-13 10:29:02 -0300

    add onClickListener for buttons

--- app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.example.memoryleakapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.memoryleakapp.R
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.memoryleakapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +13,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setView()
     }
+
+    private fun setView() = binding.run {
+        asyncTaskButton.setOnClickListener { showToast() }
+        staticAsyncTaskButton.setOnClickListener { showToast() }
+        threadButton.setOnClickListener { showToast() }
+        handlerButton.setOnClickListener { showToast() }
+        singletonButton.setOnClickListener { showToast() }
+        listenerButton.setOnClickListener { showToast() }
+        broadcastReceiverButton.setOnClickListener { showToast() }
+        staticActivityOrViewButton.setOnClickListener { showToast() }
+        innerClassButton.setOnClickListener { showToast() }
+        anonymousClassButton.setOnClickListener { showToast() }
+        timerTaskButton.setOnClickListener { showToast() }
+    }
+
+    private fun showToast() = Toast.makeText(this@MainActivity, "not implemented", Toast.LENGTH_SHORT).show()
 }
