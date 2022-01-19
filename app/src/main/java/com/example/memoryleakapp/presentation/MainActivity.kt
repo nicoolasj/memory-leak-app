51f24fac44ce4f7312aaa8b7a6c6632c129b1d49
Felipe Pelegrino <felipe.pelegrino99@gmail.com>
2022-01-19 17:54:07 -0300

    feat: removed unimplemented examples

--- app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/memoryleakapp/presentation/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.memoryleakapp.presentation
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.memoryleakapp.databinding.ActivityMainBinding
 import com.example.memoryleakapp.presentation.leaks.BroadcastReceiverActivity
@@ -22,23 +21,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setView() = binding.run {
-        asyncTaskButton.setOnClickListener { showToast() }
-        staticAsyncTaskButton.setOnClickListener { showToast() }
         threadButton.setOnClickListener { openActivity(ThreadActivity::class.java) }
-        handlerButton.setOnClickListener { showToast() }
-        singletonButton.setOnClickListener { showToast() }
-        listenerButton.setOnClickListener { showToast() }
         broadcastReceiverButton.setOnClickListener { openActivity(BroadcastReceiverActivity::class.java) }
         staticActivityOrViewButton.setOnClickListener { openActivity(StaticReferenceActivity::class.java) }
-        innerClassButton.setOnClickListener { showToast() }
-        anonymousClassButton.setOnClickListener { showToast() }
-        timerTaskButton.setOnClickListener { showToast() }
         viewsLeakButton.setOnClickListener { openActivity(ViewsLeakActivity::class.java) }
     }
 
     private fun <T: Any> openActivity(cls: Class<T>) =
         Intent(this, cls).also { startActivity(it) }
 
-    private fun showToast() =
-        Toast.makeText(this@MainActivity, "not implemented", Toast.LENGTH_SHORT).show()
 }
